e2d06f203dd8395f894469433734c133f2c53709
jonatas.santos <jonatas.santos@teladochealth.com>
2022-04-25 01:08:43 -0300

    create user services and repositories

---
+++ lib/app/repositories/user/user_repository_impl.dart.dart
@@ -0,0 +1,35 @@
+
+
+
+
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todo_list_provider/app/exception/auth_exception.dart';
+import 'package:todo_list_provider/app/repositories/user/user_repository.dart';
+
+ class UserRepositoryImpl implements UserRepository {
+final FirebaseAuth _firebaseAuth;
+UserRepositoryImpl({required FirebaseAuth firebaseAuth}): _firebaseAuth = firebaseAuth;
+
+  @override
+  Future<User?> register(String email, String password) async {
+  try {
+     final userCredencial = await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+    return userCredencial.user;
+  } on FirebaseAuthException catch (e,s) {
+  log(e.toString());
+  log(s.toString());
+  if(e.code == 'email-already-in-use'){
+    final loginTypes = await _firebaseAuth.fetchSignInMethodsForEmail(email);
+    if (loginTypes.contains('password')) {
+      throw AuthException(message: 'Email já utilizado, por favor escolha outro e-mail');
+    } else {
+      throw AuthException(message: 'Você se cadastrou no TodoList pelo Goole, por favor utilize ele para entrar!!!');
+    }
+  }else{
+      throw AuthException(message: e.message ?? 'Erro ao registrar usuário');
+  }
+  }
+  }
+}
---
+++ lib/app/services/user/user_service.dart
@@ -0,0 +1,6 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class UserService {
+  Future<User?> register(String email, String password);
+}
